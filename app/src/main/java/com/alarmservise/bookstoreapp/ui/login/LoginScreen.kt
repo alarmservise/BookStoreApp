70f5df432db79ef77fc824e0d97a232b300a1c19
Alarm <alarmservise@mail.ru>
2024-12-14 12:59:54 +0300

    end 1 lesson Books

--- app/src/main/java/com/alarmservise/bookstoreapp/ui/login/LoginScreen.kt
+++ app/src/main/java/com/alarmservise/bookstoreapp/ui/login/LoginScreen.kt
@@ -1,67 +1,97 @@
 package com.alarmservise.bookstoreapp.ui.login
 
-import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.content.MediaType.Companion.Text
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import com.google.rpc.context.AttributeContext.Auth
+import androidx.compose.ui.unit.sp
+import com.alarmservise.bookstoreapp.R
+import com.alarmservise.bookstoreapp.ui.theme.BoxFilterColor
 
 @Composable
 fun LoginScreen() {
-    val auth = Firebase.auth
-    val emailState = remember {
+
+    var emailState = remember {
         mutableStateOf("")
     }
     val passwordState = remember {
         mutableStateOf("")
     }
-    Column(
+    Image(
+        painter = painterResource(
+            id = R.drawable.book_store_bg
+        ),
+        contentDescription = "Bg",
         modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BoxFilterColor)
+
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = 50.dp,
+                end = 50.dp
+            ),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        TextField(value = emailState.value, onValueChange = {
+        Image(
+            painter =
+            painterResource(R.drawable.books),
+            contentDescription = "Logo",
+            modifier = Modifier.size(200.dp)
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(
+            text = "NECO Book Store",
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            fontFamily = FontFamily.Serif
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        RoundedCornerTextField(
+            text = emailState.value,
+            label = "Email"
+        ) {
             emailState.value = it
-        })
-        Spacer(modifier = Modifier.height(10.dp))
-
-        TextField(value = passwordState.value, onValueChange = {
-            emailState.value = it
-        })
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {
-            signUp(auth, emailState.value, passwordState.value)
-
-        }) {
-            Text(text = "Sign Up")
         }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        RoundedCornerTextField(
+            text = passwordState.value,
+            label = "Password"
+        ) {
+            passwordState.value = it
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        LoginButton(text = "Sign In") { }
+        LoginButton(text = "Sign Up") { }
 
     }
-}
-
-private fun signUp(auth: FirebaseAuth, email: String, password: String){
-    auth.createUserWithEmailAndPassword(email,password)
-        .addOnCompleteListener{
-            if (it.isSuccessful) {
-                Log.d("MyLog", "Sign Up successful!")
-            } else {
-                Log.d("MyLog","Sign Up failure!")
-            }
-
-        }
-
 }
